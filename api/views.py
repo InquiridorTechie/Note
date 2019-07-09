39cf620c1be4b34952bad7159ed906a27e3b86be
inquiridortechie <narya436.na@gmail.com>
2019-07-09 09:29:15 +0530

    feat(*): Create views and templates
    It is used to see all notes that have created so far using an HTML page.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def notes(request):
+    response = requests.get('http://127.0.0.1:8000/api/note/')
+    notes = response.json()
+    length = len(notes['objects'])
+    return render(request, 'api/home.html', {
+        'notes' : notes['objects'],
+        'len' : length
+    })
